e571fbb846958900ada0647597dd52ce52f916fa
Dinesh-Uyyala <dineshuyyala1@gmail.com>
2025-09-12 10:50:25 +0530

    all functionalities completed

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  loginUrl:'https://login-1-nkji.onrender.com/login',
+  vehiclesUrl:'https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction'
+};
